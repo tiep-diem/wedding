c90a28b2b7c33b5dc31af4d43f19aab5eab9ac6b
tungvt <thanhtung97>
2025-09-30 23:24:53 +0700

    update

--- common/js/init.js
+++ common/js/init.js
@@ -1,5 +1,5 @@
-import '/common/emoji-picker/js/index.js'
-$(".textarea-emoji-picker emoji-picker").dataSource = '/common/emoji-picker/js/data.json';
+import '/wedding/common/emoji-picker/js/index.js'
+$(".textarea-emoji-picker emoji-picker").dataSource = '/wedding/common/emoji-picker/js/data.json';
 $(document).ready(function() {
     $(document).on('emoji-click', '.textarea-emoji-picker emoji-picker', function(e){
         let formInput = $(this).parents('.textarea-emoji-picker').find("textarea");
